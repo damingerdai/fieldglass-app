a4ea1b74728cae27c8cb69798461cadd479a6536
damingerdai <mingguobin@live.com>
2025-10-11 11:13:49 +0800

    feat: update dashboard layout

--- app/(protected)/dashboard/page.tsx
+++ app/(protected)/dashboard/page.tsx
@@ -1,16 +1,3 @@
-"use client";
-
-import {
-  Sidebar,
-  SidebarInset,
-  SidebarProvider,
-} from "@/components/ui/sidebar";
-
 export default function Page() {
-  return (
-    <SidebarProvider>
-      <Sidebar />
-      <SidebarInset>hello this is dashboard view</SidebarInset>
-    </SidebarProvider>
-  );
+  return <div className="p-4">hello, this is dashboard view</div>;
 }
--- app/(protected)/layout.tsx
+++ app/(protected)/layout.tsx
@@ -1,5 +1,15 @@
 import { Navbar } from "@/components/navbar";
+import {
+  SidebarGroup,
+  SidebarGroupContent,
+  SidebarGroupLabel,
+  SidebarMenu,
+  SidebarMenuItem,
+} from "@/components/ui/sidebar";
+import { SidebarMenuButton } from "@/components/app-sidebar-button";
 import { createClient } from "@/utils/supabase/server";
+import { HomeIcon } from "lucide-react";
+import Link from "next/link";
 import { redirect } from "next/navigation";
 import { ReactNode } from "react";
 
@@ -14,7 +24,26 @@ export default async function Layout({ children }: { children: ReactNode }) {
   return (
     <>
       <Navbar title="Fieldglass App" user={data?.user} />
-      <main className="flex min-h-screen ">{children}</main>
+      <div className="flex w-full h-[calc(100vh - 64px]">
+        <aside className="flex flex-col w-56 border-r border-solid border-[#F4EEFC]">
+          <SidebarGroup>
+            <SidebarGroupLabel>Home</SidebarGroupLabel>
+            <SidebarGroupContent>
+              <SidebarMenu>
+                <SidebarMenuItem>
+                  <SidebarMenuButton asChild>
+                    <Link href="dashboard">
+                      <HomeIcon />
+                      <span>Dashboard</span>
+                    </Link>
+                  </SidebarMenuButton>
+                </SidebarMenuItem>
+              </SidebarMenu>
+            </SidebarGroupContent>
+          </SidebarGroup>
+        </aside>
+        <main>{children}</main>
+      </div>
     </>
   );
 }
